cf594560bb8af34b24aaf31159b7c8721935c2a0
Micah Rosales <micah.rosales@gmail.com>
2015-04-21 10:21:42 -0400

    Merged fix from OhMyZSH upstream for gwip alias

--- git/completion.zsh
+++ git/completion.zsh
@@ -165,8 +165,9 @@ function work_in_progress() {
     echo "WIP!!"
   fi
 }
+
 # these alias commit and uncomit wip branches
-alias gwip='git add -A; git ls-files --deleted -z | xargs -r0 git rm; git commit -m "--wip--"'
+alias gwip='git add -A; git rm $(git ls-files --deleted); git commit -m "--wip--"'
 alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
 
 # these alias ignore changes to file
