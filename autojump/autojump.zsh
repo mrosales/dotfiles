0df8d1ad4fbc611b619a98d866d94214fca9eda1
Micah Rosales <micah@unify.id>
2020-02-24 18:40:24 -0600

    fix autojump looking for wrong command

--- autojump/autojump.zsh
+++ autojump/autojump.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 # Loads autojump plugin
-if (( $+commands[brew] )) ; then
+if [ $commands[autojump] ] ; then
   [[ -s /usr/local/etc/autojump.sh ]] && . /usr/local/etc/autojump.sh
 fi
